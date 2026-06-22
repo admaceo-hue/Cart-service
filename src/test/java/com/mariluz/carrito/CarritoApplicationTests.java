48a338385f0ade34fbe641bb3caae4e7db4d0fd9
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-21 22:53:04 -0400

    fix: fix para test contextLoads agregando propiedad de test para jwt.secret

--- src/test/java/com/mariluz/carrito/CarritoApplicationTests.java
+++ src/test/java/com/mariluz/carrito/CarritoApplicationTests.java
@@ -3,18 +3,21 @@ package com.mariluz.carrito;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-	"spring.datasource.url=jdbc:h2:mem:testdb",
-	"spring.datasource.driverClassName=org.h2.Driver",
-	"spring.datasource.username=sa",
-	"spring.datasource.password=password",
-	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-	"spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest(
+    properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "jwt.secret=98PAzqkLUAElxtt+iNCg3GxPvqHI8ytzNfilW2S3RhQ=",
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+    }
+)
 class CarritoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
